ab54a1b75f366f2d532e0e40042fa9cbb505daa8
Ram Vanga <rama.vanga@ringcentral.com>
2021-11-12 14:41:32 -0800

    refactor

--- DSA/src/ram/dsa/stacks/NearestGreatertoLeft.java
+++ DSA/src/ram/dsa/stacks/NearestGreatertoLeft.java
@@ -8,7 +8,6 @@ import java.util.Stack;
 //NGL
 public class NearestGreatertoLeft {
 
-
     public static void main(String[] args) {
         int[] arr = {1, 3, 0, 0,1,2,4};
         System.out.println(Arrays.toString(find(arr)));
